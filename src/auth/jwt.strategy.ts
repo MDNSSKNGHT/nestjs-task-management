994eca6d4bdbd0abf3b1efa7dda962ef0b1ca286
Leonardo Estacio <mdnssknght@gmail.com>
2025-09-21 15:55:36 -0500

    Add JWT secret to config validation schema

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -5,14 +5,16 @@ import { Repository } from 'typeorm';
 import { User } from './user.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { JwtPayload } from './jwt-payload.interface';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor(
     @InjectRepository(User) private usersRepository: Repository<User>,
+    private configService: ConfigService,
   ) {
     super({
-      secretOrKey: 'topSecret51',
+      secretOrKey: configService.get('JWT_SECRET') as string, // Workaround
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
     });
   }
